7ef0d1148a0421b12cd446a99e97e85670397ddb
gabriel <gabriel.joaquim.5567@gmail.com>
2023-08-22 20:03:43 -0300

    feat: Exercicios 9, 12, 13 e 14 adicionados
    Adicionei os exercicios 9, 12, 13 e 14 ao menu contendo todos os outros da lista.

--- menu.cpp
+++ menu.cpp
@@ -159,6 +159,28 @@ int main()
 				break;
 				
 			case 9:
+				int v9[5], i9, j9, temp9;
+				v9[0]=45;
+				v9[1]=-89;
+				v9[2]=32;
+				v9[3]=-12;
+				v9[4]=33;
+
+    			for (i9 = 0; i9 < 5- 1; i9++) {
+        			for (j9 = 0; j9 < 5 - i9 - 1; j9++) {
+            			if (v9[j9] > v9[j9 + 1]) {
+                			temp9 = v9[j9];
+                			v9[j9] = v9[j9 + 1];
+                			v9[j9 + 1] = temp9;
+            			}
+        			}
+    			}
+
+    			printf("\nVetor ordenado de forma crescente: \n");
+    			for (int i9 = 0; i9 < 5; i9++) {
+        			printf("%d \n", v9[i9]);
+    			}
+    			
 				break;
 				
 			case 10:
@@ -199,13 +221,114 @@ int main()
 				
 				break;
 				
-			case 12:
+			case 12:			
+				int v12[5], i12, j12, temp12;
+				v12[0]=41;
+				v12[1]=72;
+				v12[2]=39;
+				v12[3]=4;
+				v12[4]=35;
+
+    			for ( i12 = 0; i12 < 5- 1; i12++) {
+        			for (j12 = 0; j12 < 5 - i12 - 1; j12++) {
+            			if (v12[j12] > v12[j12 + 1]) {
+                			temp12 = v12[j12];
+                			v12[j12] = v12[j12 + 1];
+                			v12[j12 + 1] = temp12;
+            			}
+        			}
+    			}
+			
+    			printf("\nVetor ordenado de forma crescente: \n");
+    			for (int i12 = 0; i12 < 5; i12++) {
+        			printf("%d \n", v12[i12]);
+    			}
 				break;
 				
 			case 13:
+				int v13[5], tamanho, i13, indice_maximo13, j13;
+				v13[0]=41;
+				v13[1]=72;
+				v13[2]=39;
+				v13[3]=4;
+				v13[4]=35;
+    			tamanho = 5;
+
+    			for (i13 = 0; i13 < 5 - 1; i13++) {
+        			indice_maximo13 = i13;
+        			for (j13 = i13 + 1; j13 < 5; j13++) {
+            			if (v13[j13] > v13[indice_maximo13]) {
+                			indice_maximo13 = j13;
+            			}
+        			}
+        			if (indice_maximo13 != i13) {
+            			int temp13 = v13[i13];
+            			v13[i13] = v13[indice_maximo13];
+            			v13[indice_maximo13] = temp13;
+        			}
+    			}
+			
+    			printf("\nVetor ordenado de forma decrescente: \n");
+    			for (int i13 = 0; i13 < 5; i13++) {
+        			printf("%d\n ", v13[i13]);
+    			}
 				break;
-				
+							
 			case 14:
+				int v14[5], i14, aux14,j14;
+				v14[0]=41;
+				v14[1]=72;
+				v14[2]=39;
+				v14[3]=4;
+				v14[4]=35;
+				i14=0;
+				char op14;
+				
+				printf("Em qual ordem voce deseja ver o vetor?[c]crescente e [d] decrescente: ");
+					scanf(" %c", &op14);
+			
+					
+				if ( op14 == 'c' ){
+				for(i14=0; i14<5; i14++){
+					for( j14= i14+1; j14<5; j14++){
+						if(v14[i14]>v14[j14]){
+					
+						aux14 = v14[i14];
+						v14[i14] = v14[j14];
+						v14[j14] = aux14;	
+						
+						}
+					}
+					
+		
+			}	 
+				printf("\nOrdem crescente:\n");	
+					for( i14=0; i14<5; i14++){
+					printf("%d\n ", v14[i14]);
+				}
+			}
+						
+				if ( op14 == 'd' ){
+				for(i14=0; i14<5; i14++){
+					for(i14=0; i14<5; i14++){
+					for( j14= i14+1; j14<5; j14++){
+						if(v14[i14]<v14[j14]){
+					
+						aux14 = v14[i14];
+						v14[i14] = v14[j14];
+						v14[j14] = aux14;	
+						
+						}
+					}
+					
+				
+			}	 printf("\nA ordem eh:\n ");	
+				
+				for( i14=0; i14<5; i14++){
+					printf("%d \n", v14[i14]);
+	 			}
+			}
+			}
 				break;
 				
 			case 15:
